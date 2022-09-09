80ff1d7e265e88761f53300eea1b6560dd5e178c
Rob Sassack <rsassack25@gmail.com>
2022-09-09 19:31:14 -0400

    Fixing assignment 1

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,29 @@
+function fibs(n) {
+  let output = [];
+  let index = 0;
+  while (index < n) {
+    if (index === 0) {
+      output.push(0);
+    } else if (index === 1) {
+      output.push(1);
+    } else {
+      output.push(output[index-1] + output[index-2])
+    }
+    index++;
+  }
+  return output;
+}
+
+console.log(fibs(20));
+
+function fibsRec(n) {
+  let output = [];
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return output.push(fibsRec(n-1));
+}
+
+console.log(fibs(20));
